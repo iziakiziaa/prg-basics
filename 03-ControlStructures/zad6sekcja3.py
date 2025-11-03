5cee22f05193b17dfb5469acbf83ad83ff6a1f9d
Izabela Szwedowska <izabelaszwedowska@gmail.com>
2025-11-03 18:06:27 +0100

    yyyyy

--- 03-ControlStructures/zad6sekcja3.py
+++ 03-ControlStructures/zad6sekcja3.py
@@ -11,9 +11,12 @@ if month==1 or month==3 or month==5 or month==7 or month==9 or month==10 or mont
 elif month==4 or month==6 or month==8 or month==11:
     if day >=1 and day <= 30:
         day_ok = True
+elif month==2:
+    if day >=1 and day <= 28:
+        day_ok = True
 
 message = f'Day {...} for the month {...}'
 if day_ok:
     print('{message} is correct')
 else
-    print(...)
+    print('{message} is incorrect')
