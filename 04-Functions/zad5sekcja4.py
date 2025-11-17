c2132e99af600a993ff8c199b7cbe05782d53a2c
Izabela Szwedowska <izabelaszwedowska@gmail.com>
2025-11-17 18:05:48 +0100

    cos tam nie cos

--- 04-Functions/zad5sekcja4.py
+++ 04-Functions/zad5sekcja4.py
@@ -4,12 +4,18 @@
 #
 def pts_to_grade(points):
     grade = ''
+
     if points >= 18:
         grade = 'Excellent'
-    ...
-    ...
+    elif points >= 14:
+        grade = 'Good'
+    elif points >=10: 'Satisfactory'
+
+    else: 
+        grade= 'Fail'
+
     return grade
 
-test_result = 15
+test_result = 9
 final_grade = pts_to_grade(test_result)
-print('You scored ... points on the test. Your final grade is ...')
+print(f'You scored {test_result} points on the test. Your final grade is {final_grade}')
